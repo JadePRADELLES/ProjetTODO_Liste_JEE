909f5141c2e97e98ae5bd1ccf7ea5420055eb93e
jade <jadepradelles@gmail.com>
2022-04-23 11:29:14 +0200

    Début

---
+++ src/main/java/com/pradelles/todoliste/TodoListeJeeApplication.java
@@ -0,0 +1,13 @@
+package com.pradelles.todoliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListeJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListeJeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradelles/todoliste/TodoListeJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradelles.todoliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListeJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
