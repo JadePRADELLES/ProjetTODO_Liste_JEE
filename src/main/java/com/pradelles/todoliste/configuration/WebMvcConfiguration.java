967319f88706854959849db323141aa98ff924d0
jade <jadepradelles@gmail.com>
2022-05-03 23:18:15 +0200

    Bootstrap:descrition et style page acceuil.html

--- src/main/java/com/pradelles/todoliste/configuration/WebMvcConfiguration.java
+++ src/main/java/com/pradelles/todoliste/configuration/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.pradelles.todoliste.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login").setViewName("connection");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("styleBootstrap/jquery/**").addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.3.1-1/");
+		registry.addResourceHandler("styleBootstrap/popper/**").addResourceLocations("classpath:/META-INF/resources/webjars/popper.js/1.14.1/umd/");
+		registry.addResourceHandler("styleBootstrap/bootstrap/**").addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.1.1/");
 	}
 
 }
